0798f8337b5269c2c86b1404bd6a03e8ff7de4a3
Seedak <spstaneja@gmail.com>
2018-06-23 22:03:23 +0530

    Sorting

---
+++ Arrays/Sorting/MinimumAbsoluteDiff.java
@@ -0,0 +1,26 @@
+// java implementation to find the sum
+// of minimum absolute difference of
+// each array element
+import java.*;
+import java.util.Arrays;
+
+public class MinimumAbsoluteDiff{
+	static int sumOfMinAbsDifferences(	int arr[] ,int n){
+		Arrays.sort(arr);
+		int sum = 0;
+		sum += Math.abs(arr[0] - arr[1]);
+		sum += Math.abs(arr[n-1] - arr[n-2]);
+		for (int i = 1; i < n - 1; i++)
+			sum += Math.min(Math.abs(arr[i] - arr[i-1]),
+					Math.abs(arr[i] - arr[i+1]));
+		return sum; 
+	}	 
+	public static void main(String args[])
+	{
+		int arr[] = {5, 10, 1, 4, 8, 7};
+		int n = arr.length;
+		
+		System.out.println( "Sum = "
+		+ sumOfMinAbsDifferences(arr, n));
+	}
+}
